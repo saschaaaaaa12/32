5ed3d1618417385bb8ed4314d81f4028bc54a088
saschaaaaaa12 <sashopanteleev@yandex.ru>
2023-11-28 07:32:27 +0700

    32

---
+++ 32/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _32
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        void PrintRTBContent(Object sender, RoutedEventArgs args)
+        {
+            PrintCommand();
+        }
+
+       
+        private void PrintCommand()
+        {
+            PrintDialog pd = new PrintDialog();
+            if ((pd.ShowDialog() == true))
+            {
+                pd.PrintVisual(richTB as Visual, "printing as visual");
+                pd.PrintDocument((((IDocumentPaginatorSource)richTB.Document).DocumentPaginator), "printing as paginator");
+            }
+        }
+
+
+    }
+}
